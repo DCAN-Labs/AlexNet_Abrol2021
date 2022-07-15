1bcb195d5ef455d98b6fc7499c90a17378a8673b
paul-reiners <paul.reiners@gmail.com>
2022-07-15 11:41:30 -0500

    Rewrote get_candidate_info_list so it makes sense.

--- src/dcan/dsets/loes_scoring/dsets.py
+++ src/dcan/dsets/loes_scoring/dsets.py
@@ -17,8 +17,7 @@ raw_cache = getCache('dcan_loes_score')
 
 CandidateInfoTuple = namedtuple(
     'CandidateInfoTuple',
-    '''ald_code_mrn_uid, ald_code_uid, mrn_int, ses_date, loes_score_pre_transplant_int, dmri_12dir_path_str, '''
-    '''mprage_path_str'''
+    'loes_score_pre_transplant_int, ald_code_ses_uid, ald_code_str, mrn_int, ses_date',
 )
 
 
@@ -39,38 +38,36 @@ def get_candidate_info_list(require_on_disk_bool=True):
     # We construct a set with all ald_code_uids that are present on disk.
     # This will let us use the data, even if we haven't downloaded all of
     # the subsets yet.
-    dmri_12dir_mri_list = glob.glob(
-        '/home/feczk001/shared/data/loes_scoring/Loes_score_validated/sub-*/ses-*/dmri_12dir.nii.gz')
+    loes_score_validated_path = '/home/feczk001/shared/data/loes_scoring/Loes_score_validated/sub-*/ses-*/'
+    nifti_ext = '.nii.gz'
+    dmri_12dir_mri_list = glob.glob(f'{loes_score_validated_path}dmri_12dir{nifti_ext}')
     dmri_12dir_present_on_disk_set = {get_uid(p) for p in dmri_12dir_mri_list}
-    present_on_disk_set = {}
+    mprage_mri_list = glob.glob(f'{loes_score_validated_path}mprage{nifti_ext}')
+    mprage_present_on_disk_set = {get_uid(p) for p in mprage_mri_list}
+    present_on_disk_set = dmri_12dir_present_on_disk_set.intersection(mprage_present_on_disk_set)
 
     candidate_info_list = []
-    with open('data/loes_scoring/validated_loes_scores.csv', "r") as f:
+    scores_csv = '/home/miran045/reine097/projects/AlexNet_Abrol2021/data/loes_scoring/validated_loes_scores.csv'
+    with open(scores_csv, "r") as f:
         for row in list(csv.reader(f))[1:]:
             ses_str = row[1].strip()
             mrn_str = row[2].strip()
-            ald_code_uid = row[0]
-            ald_code_ses_mrn_uid = '_'.join([ald_code_uid, ses_str, mrn_str])
+            ald_code_str = row[0]
+            ald_code_ses_uid = '_'.join([ald_code_str, ses_str])
 
-            if ald_code_ses_mrn_uid not in present_on_disk_set and require_on_disk_bool:
+            if ald_code_ses_uid not in present_on_disk_set and require_on_disk_bool:
                 continue
 
             mrn_int = int(mrn_str)
             loes_score_pre_transplant_int = int(row[3])
-            base_dir = '/home/feczk001/shared/data/loes_scoring/Loes_score_validated'
-            session_dir = os.path.join(base_dir, f'sub-{ald_code_uid}', f'ses-{ses_str}')
-            dmri_12dir_path_str = os.path.join(session_dir, 'dmri_12dir.nii.gz')
-            mprage_path_str = os.path.join(session_dir, 'mprage.nii.gz')
             ses_date = datetime.strptime(ses_str, '%Y%m%d')
 
             candidate_info_list.append(CandidateInfoTuple(
-                ald_code_ses_mrn_uid,
-                ald_code_uid,
+                loes_score_pre_transplant_int,
+                ald_code_ses_uid,
+                ald_code_str,
                 mrn_int,
                 ses_date,
-                loes_score_pre_transplant_int,
-                dmri_12dir_path_str,
-                mprage_path_str,
             ))
 
     candidate_info_list.sort(reverse=True)
