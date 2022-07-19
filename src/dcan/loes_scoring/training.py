9a361f85828eae24278ea11c06b95bf5f8a8cf9a
paul-reiners <paul.reiners@gmail.com>
2022-07-19 15:18:41 -0500

    Adding training for Loes scoring.

--- src/dcan/loes_scoring/training.py
+++
@@ -1,9 +0,0 @@
-from dcan.training import InfantMRITrainingApp
-
-
-class InfantMRILoesScoringTrainingApp(InfantMRITrainingApp):
-    pass
-
-
-if __name__ == '__main__':
-    InfantMRITrainingApp().main()
