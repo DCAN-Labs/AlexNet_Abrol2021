5544777cb33a46bab5fc78912138ab5ba5a58d2f
paul-reiners <paul.reiners@gmail.com>
2022-09-20 08:56:35 -0500

    Filling in missing total scores.

--- src/dcan/loes_scoring/eda/compare_full_versus_partial_scores.py
+++ src/dcan/loes_scoring/eda/compare_full_versus_partial_scores.py
@@ -1,5 +1,7 @@
 import os.path
 import pandas as pd
+import numpy as np
+
 
 from dcan.loes_scoring.data.partial_loes_scores import get_partial_loes_scores
 
@@ -8,12 +10,17 @@ partial_scores_file_path = os.path.join(loes_scoring_folder, '9_7 MRI sessions I
 partial_loes_scores = get_partial_loes_scores(partial_scores_file_path)
 full_score_path = os.path.join(loes_scoring_folder, 'ALD-google_sheet-Jul272022-Loes_scores-Igor.csv')
 full_scores_df = pd.read_csv(full_score_path)
-unscored_df = full_scores_df[full_scores_df['Loes score'].isna()]
-for ind in unscored_df.index:
-    sub_id = unscored_df['_Sub ID'][ind]
+for ind in full_scores_df.index:
+    loes_score_field_name = 'Loes score'
+    score = full_scores_df[loes_score_field_name][ind]
+    if not np.isnan(score):
+        continue
+    sub_id = full_scores_df['_Sub ID'][ind]
     if sub_id in partial_loes_scores:
-        sub_session = unscored_df['Sub_Session'][ind]
+        sub_session = full_scores_df['Sub_Session'][ind]
         if sub_session in partial_loes_scores[sub_id]:
             partial_score_score = partial_loes_scores[sub_id][sub_session]
             if partial_score_score.loes_score != 0:
-                print(partial_score_score.loes_score)
+                full_scores_df.at[ind, loes_score_field_name] = partial_score_score.loes_score
+updated_full_score_path = os.path.join(loes_scoring_folder, 'ALD-google_sheet-Jul272022-Loes_scores-Igor_updated.csv')
+full_scores_df.to_csv(updated_full_score_path, index=False)
