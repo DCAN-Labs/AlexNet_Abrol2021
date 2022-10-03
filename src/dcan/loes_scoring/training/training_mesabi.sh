82b6e25a99d82ed3942b2b1847d5321c6142299d
paul-reiners <paul.reiners@gmail.com>
2022-10-03 16:42:42 -0500

    Shell script for training.

--- src/dcan/loes_scoring/training/training_mesabi.sh
+++ src/dcan/loes_scoring/training/training_mesabi.sh
@@ -2,10 +2,10 @@
 
 #SBATCH --job-name=loes-scoring-alex-net # job name
 
-#SBATCH --mem=90g        # memory per cpu-core (what is the default?)
+#SBATCH --mem=180g        # memory per cpu-core (what is the default?)
 #SBATCH --time=24:00:00          # total run time limit (HH:MM:SS)
 #SBATCH -p v100
-#SBATCH --gres=gpu:v100:1
+#SBATCH --gres=gpu:v100:2
 #SBATCH --ntasks=6               # total number of tasks across all nodes
 
 #SBATCH --mail-type=begin        # send 7mail when job begins
@@ -19,5 +19,5 @@
 cd /home/miran045/reine097/projects/AlexNet_Abrol2021 || exit
 export PYTHONPATH=PYTHONPATH:"/home/miran045/reine097/projects/AlexNet_Abrol2021/src"
 /home/miran045/reine097/projects/AlexNet_Abrol2021/venv/bin/python \
-  /home/miran045/reine097/projects/AlexNet_Abrol2021/src/dcan/loes_scoring/training/training.py --batch-size=1 --epochs=512 \
-  --num-workers=6
+  /home/miran045/reine097/projects/AlexNet_Abrol2021/src/dcan/loes_scoring/training/training.py --batch-size=1 --epochs=768 \
+  --num-workers=6 --csv-data-file="/home/miran045/reine097/projects/AlexNet_Abrol2021/data/loes_scoring/ALD-google_sheet-Jul272022-Loes_scores-Igor_updated.csv"
