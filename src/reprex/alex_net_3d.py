7977babe9c79461c2f6177b54a6256e4105e6232
paul-reiners <paul.reiners@gmail.com>
2023-02-21 20:19:50 -0600

    Created TensorFlow version of AlexNet.  Working now.

--- src/reprex/alex_net_3d.py
+++ src/reprex/alex_net_3d.py
@@ -3,7 +3,8 @@ from tensorflow import keras
 
 def get_alex_net():
     return keras.models.Sequential([
-        keras.layers.Conv3D(filters=96, kernel_size=(11, 11, 11), strides=(4, 4, 4), activation='relu'),
+        keras.layers.Conv3D(filters=96, kernel_size=(11, 11, 11), strides=(4, 4, 4), activation='relu',
+                            input_shape=(197, 233, 189, 1)),
         keras.layers.BatchNormalization(),
         keras.layers.MaxPool3D(pool_size=(3, 3, 3), strides=(2, 2, 2)),
         keras.layers.Conv3D(filters=256, kernel_size=(5, 5, 5), strides=(1, 1, 1), activation='relu', padding="same"),
