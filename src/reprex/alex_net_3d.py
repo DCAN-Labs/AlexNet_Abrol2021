cd7ca0875892dc77b7cb28ee100d5677a9d05a49
paul-reiners <paul.reiners@gmail.com>
2023-02-23 15:50:38 -0600

    Successfully training on TensorFlow AlexNet with our data.

--- src/reprex/alex_net_3d.py
+++ src/reprex/alex_net_3d.py
@@ -1,10 +1,10 @@
 from tensorflow import keras
 
 
-def get_alex_net():
+def get_alex_net(width=128, height=128, depth=64):
     return keras.models.Sequential([
         keras.layers.Conv3D(filters=96, kernel_size=(11, 11, 11), strides=(4, 4, 4), activation='relu',
-                            input_shape=(197, 233, 189, 1)),
+                            input_shape=(width, height, depth, 1)),
         keras.layers.BatchNormalization(),
         keras.layers.MaxPool3D(pool_size=(3, 3, 3), strides=(2, 2, 2)),
         keras.layers.Conv3D(filters=256, kernel_size=(5, 5, 5), strides=(1, 1, 1), activation='relu', padding="same"),
@@ -22,5 +22,5 @@ def get_alex_net():
         keras.layers.Dropout(0.5),
         keras.layers.Dense(4096, activation='relu'),
         keras.layers.Dropout(0.5),
-        keras.layers.Dense(units=1)
+        keras.layers.Dense(units=1, activation="sigmoid")
     ])
